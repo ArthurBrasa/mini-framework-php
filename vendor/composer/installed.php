385d07de97db7b3031516dfa8a5b8aadf2f57cb8
Arthur Brasa <arthurbrasa@outlook.com>
2023-08-17 18:48:52 -0300

    Instruções para rodar o projeto

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'min-framework/php',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'mini-framework/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '262e20d7f15c3b0de7d25074fa20549f25f8db7b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'min-framework/php' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'mini-framework/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '262e20d7f15c3b0de7d25074fa20549f25f8db7b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
